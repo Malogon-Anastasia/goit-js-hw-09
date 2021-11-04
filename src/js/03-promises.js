3dd948e9b01fd6737d0f43f0df46a9f9fea70de2
Malogon Anastasua <malogonanastasia@gmail.com>
2021-11-04 16:04:24 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,40 +1,35 @@
 import { Notify } from 'notiflix';
 
 const refs = {
-  form: document.querySelector('.form'),
+  delayField: document.querySelector('input[name="delay"]'),
+  stepField: document.querySelector('input[name="step"]'),
+  amountField: document.querySelector('input[name="amount"]'),
   btn: document.querySelector('.js-btn'),
-};
-
-let isActive = false;
+  };
 
 refs.btn.addEventListener('click', onBtnClick);
 
 function onBtnClick(e) {
   e.preventDefault();
-  const { delay, step, amount } = Object.fromEntries(new FormData(refs.form));
-  if (isActive) {
-    return;
-  }
-  disableAlerter(delay, step, amount);
-  for (let i = 1; i <= amount ; i += 1) {
-    createPromise(i, Number(delay) + Number(step) * i)
-      .then(onSuccess)
-      .catch(onError);
-  }
+  const { delayField, stepField, amountField } = refs;
+  
+  const amount = +amountField.value;
+  const step = +stepField.value;
+  let delay = +delayField.value;
+  
+  for (let i = 0; i < amount ; i ++) {
+    const position = i + 1;
+    createPromise(position, delay)
+    .then(({ position, delay }) => {
+      Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    });
+  delay += step;
 }
-function onSuccess({ position, delay }) {
-  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { clickToClose: true });
 }
 
-function onError({ position, delay }) {
-  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { clickToClose: true });
-}
-
-
-function disableAlerter(delay, step, amount) {
-  isActive = true;
-  setTimeout(() => (isActive = false), Number(delay) + Number(step) * amount);
-}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
